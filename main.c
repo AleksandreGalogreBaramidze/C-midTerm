5cef1a9091d0edde0ffc96c9e496785ee5858563
Aleksandre Galogre-Baramidze <74540578+AleksandreGalogreBaramidze@users.noreply.github.com>
2022-04-10 11:28:44 +0400

    final_version

--- main.c
+++ main.c
@@ -4,5 +4,26 @@
 
 int main() 
 {
+    int target_char;
+    int max;
+    char lines[MAXLINE];
+    char mostTargetedChars[MAXLINE];
+    
+    max = 0;
+
+    while ((target_char = punctCount(lines, MAXLINE)) > 0)
+        if (target_char > max) 
+
+        {
+            max = target_char;
+            copy(mostTargetedChars, lines);
+        }
+
+    if (max > 0)
+    {
+        printf("\nline: %s\n", mostTargetedChars);
+    }
+
+    return 0;
 
 }
